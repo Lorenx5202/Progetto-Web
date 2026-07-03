e81da98b5c647844bdab8baabb24a3c8c193b750
ErTrudy67 <84097059+ErTrudy67@users.noreply.github.com>
2026-07-04 00:48:54 +0200

    Update Consumatore.java

--- Consumatore.java
+++ Consumatore.java
@@ -1,81 +1,89 @@
-public class Cliente {
+public class Consumatore {
+    final LVL_PRIVILEGE = 1;
+    
+    private Integer id;
     private String nome;
     private String cognome;
-    private long id;
-    private String telefono;
     private String email;
+    private String numTelefono;
     private String indirizzo;
-    private String hashedPassword;
-    private int permesso;
+    private String infoAggiuntive;
+    private String hashedPass;
+    private Integer idMenu;
 
-    public Cliente(String nome, String cognome, long id, String telefono, String email,String indirizzo, String hashedPassword){
-        this.nome= nome;
-        this.cognome= cognome;
-        this.id= id;
-        this.telefono= telefono;
-        this.email= email;
-        this.indirizzo= indirizzo;
-        this.hashedPassword= hashedPassword;
-        this.permesso=1;
+    public Cliente(Integer id, String nome, String cognome, String email, String numTelefono, String indirizzo, String infoAggiuntive, String hashedPass, Integer idMenu) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.numTelefono = numTelefono;
+        this.indirizzo = indirizzo;
+        this.infoAggiuntive = infoAggiuntive;
+        this.hashedPass = hashedPass;
+        this.idMenu = idMenu;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getIndirizzo(){
-        return indirizzo;
-    }
-
-    public String getHashedPassword() {
-        return hashedPassword;
-    }
-
-    public int getPermesso(){
-        return permesso;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public String getCognome() {
+        return cognome;
+    }
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public String getNumTelefono() {
+        return numTelefono;
+    }
+    public void setNumTelefono(String numTelefono) {
+        this.numTelefono = numTelefono;
     }
 
-    public void setEmail(String email){
-        this.email= email;
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
     }
 
-    public void setIndirizzo(String indirizzo){
-        this.indirizzo= indirizzo;
+    public String getInfoAggiuntive() {
+        return infoAggiuntive;
+    }
+    public void setInfoAggiuntive(String infoAggiuntive) {
+        this.infoAggiuntive = infoAggiuntive;
     }
 
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
+    public String getHashedPass() {
+        return hashedPass;
     }
+    public void setHashedPass(String hashedPass) {
+        this.hashedPass = hashedPass;
+    }
+
+    public Integer getIdMenu() {
+        return idMenu;
+    }
+    public void setIdMenu(Integer idMenu) {
+        this.idMenu = idMenu;
+    }
+
 }
