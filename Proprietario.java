316f6f0fc088eca4b5da13d2fc20b859cd0fde3d
ErTrudy67 <84097059+ErTrudy67@users.noreply.github.com>
2026-07-04 00:51:14 +0200

    Update Proprietario.java

--- Proprietario.java
+++ Proprietario.java
@@ -1,31 +1,38 @@
 public class Proprietario {
+    final int LVL_PRIVILEGE = 3;
+
+    private Integer id;
     private String nome;
     private String cognome;
-    private int permesso;
 
-    public Proprietario(String nome, String cognome){
-        this.nome= nome;
-        this.cognome= cognome;
-        this.permesso= 3;
+    public Proprietario(Integer id, String nome, String cognome) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getCognome() {
-        return cognome;
-    }
-
-    public int getPermesso() {
-        return permesso;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
     }
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
+}
 }
