5746e2cf7e9468a4a41a5a6f4bdd5ab24efedb17
ErTrudy67 <84097059+ErTrudy67@users.noreply.github.com>
2026-07-04 00:53:22 +0200

    Update Personale.java

--- Personale.java
+++ Personale.java
@@ -1,41 +1,38 @@
-public class Personale{
+public class Personale {
+    final int LVL_PRIVILEGE = 2;
+
+    private Integer id;
     private String nome;
     private String cognome;
-    private int id;
-    private int permesso;
 
-    public Personale(String nome, String cognome, int id){
-        this.nome= nome;
-        this.cognome= cognome;
-        this.id= id;
-        this.permesso= 2;
+    public Personale(Integer id, String nome, String cognome) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getCognome() {
-        return cognome;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getPermesso(){
-        return permesso;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
     }
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 }
